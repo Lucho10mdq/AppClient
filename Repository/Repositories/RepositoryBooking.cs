474f0d10900a3adbeaabf1df770d353dd60cb6c4
LAPTOP-4RC7FOER <luchomdq10@gmail.com>
2018-09-26 10:02:42 -0300

    Se agrego metodo de agregar booking

--- Repository/Repositories/RepositoryBooking.cs
+++ Repository/Repositories/RepositoryBooking.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using Repository;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace Repository
 {
-    class RepositoryBooking
+    public class RepositoryBooking :Singleton<RepositoryBooking>, IBookingcs
     {
+        private List<Booking> ListBooking = new List<Booking>();
+        static RepositoryBooking instance = null;
+        SqlConnection conexion = new SqlConnection(@"Server=LAPTOP-4RC7FOER;Database=Clientes;Trusted_Connection=True;");
+
+        public int AddBooking(Booking oBooking)
+        {
+            conexion.Open();
+            SqlCommand c = conexion.CreateCommand();
+            c.Connection = conexion;
+            c.CommandType = CommandType.StoredProcedure;
+            c.CommandText = "Add_hiring";
+            c.Parameters.AddWithValue("idClient", oBooking.IdClient);
+            c.Parameters.AddWithValue("idTouristPackage", oBooking.IdTouristPackage);
+            c.Parameters.AddWithValue("hiringDatetime", oBooking.BookingDate);
+
+            int AffectedRows = c.ExecuteNonQuery();
+            conexion.Close();
+            return AffectedRows;
+
+        }
+
+        public int ModifyBooking(Booking oBooking)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Service/ServiceBooking.cs
+++ Service/ServiceBooking.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using Repository;
 
 namespace Service
 {
-    class ServiceBooking
+    public class ServiceBooking
     {
+        private RepositoryBooking ListBooking = RepositoryBooking.GetInstance();
+
+        public void AddBooking(int idClient,int idTourist,DateTime BookingDate)
+        {
+            Booking oBooking = new Booking(idClient, idTourist, BookingDate);
+            ListBooking.AddBooking(oBooking);
+        }
     }
 }
